d94dd6991283be5728182a658e0259fd50a8aebd
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-25 04:04:45 +0600

    added all file update

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+// const person = {
+//     name: "hero"
+// }
+// console.log(person);
+
+
+// document.getElementById("mybtn").addEventListener("click", () => {
+//     alert("Button Clicked");
+// });
+
+
+// let obj1 = {x : 1};
+// let obj2 = {x : 1};
+// obj1.y = 3;
+// console.log(obj1);
+// console.log(obj2);
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+// add(1, 2);
+// add("1", "2");
+
+
+
+// function getX(obj) { return obj.x; }
+
+// // First call: Slow (lookup 'x')
+// getX({ x: 1 });  
+
+// // Later calls: Fast (cached location)
+// getX({ x: 2 });  
+
