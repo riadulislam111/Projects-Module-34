b550618d38b988b57ef85a7ab9dd910b4e655063
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-08-03 20:37:26 +0600

    added all file update

---
+++ scripts/setInterval.js
@@ -0,0 +1,64 @@
+// // console.log(1)
+// // console.log(2)
+// // console.log(3)
+// let count = 0;
+
+// const clockID = setInterval(() => {
+//     count++;
+//     console.log(count);
+
+//     if (count >= 5) {
+//         clearInterval(clockID);
+//     }
+// }, 1000)
+
+// console.log(clockID);
+// // console.log(4)
+// // console.log(5)
+// // console.log(6)
+
+
+// // setTimeout(() => {
+// //       console.log("hello")
+// // },3000)
+
+
+
+// let count = 0;
+
+// const clockID = setInterval(() => {
+//     count++;
+//     console.log(count);
+
+//     if (count >= 5) {
+//         clearInterval(clockID);
+//     }
+// },200)
+
+
+// let count = 0;
+
+// const clockID = setInterval(() => {
+//     count++;
+//     console.log(count);
+
+//     if (count >= 5) {
+//         clearInterval(clockID);
+//     }
+// }, 200);
+
+
+
+// let count2 = 0;
+
+// const clockID2 = setInterval(() => {
+//     count2++;
+//     console.log(count2);
+
+//     if (count2 >= 10) {
+//         clearInterval(clockID2);
+//     }
+// },2000)
+
+
+
