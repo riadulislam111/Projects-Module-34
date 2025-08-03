f45a69bde33ed50755d831778720088806b37e35
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-08-03 22:35:31 +0600

    added  all file update

--- scripts/eventLoop.js
+++ scripts/eventLoop.js
@@ -83,3 +83,87 @@
 //     a();
 // }
 
+// console.log("Start");
+
+// setTimeout(() => {
+//     console.log("Timeout 1");
+// },1000);
+
+// setTimeout(() => {
+//     console.log("Timeout 2");
+// }, 0);
+
+// console.log("End");
+
+
+// console.log("Start");
+
+// setTimeout(() => {
+// console.log("Timeout 1");
+// }, 0);
+
+
+// Promise.resolve().then(() => {
+//     console.log("Promise 1");
+// });
+
+
+// console.log("END");
+
+
+// setTimeout(() => {
+//     console.log("This runs after 2 seconds");
+// }, 2000);
+
+// const count = 0;
+// const clockID =setInterval(() => {
+//     count++;
+//     console.log(`This runs ${count} seconds after the last run`)
+//     // console.log("This runs every 3 seconds");
+    
+// }, 3000);
+
+
+
+
+
+// const TimeoutID = setTimeout(() => {
+//     console.log("This runs after 2 seconds");
+
+// }, 2000)
+
+//     clearTimeout(TimeoutID);
+
+
+// const intervalId = setInterval(() => {
+//     console.log("This runs repeatedly");
+// }, 2000);
+
+// setTimeout(() => {
+//     clearInterval(intervalId)
+// }, 5000);
+
+
+// // setTimeout(() => {
+// //     console.log("Task started");
+
+// //     for (let i = 0; i < 1e8; i++) {}
+// //         console.log("Task finished");
+    
+// // }, 2000);
+//
+
+
+// const timeoutID = setTimeout(() => {
+//     console.log("Task started");
+
+
+//     for (let i = 0; i < 18; i++) {}
+//     console.log("Task finished");
+    
+// }, 2000);
+
+
+// clearTimeout(timeoutID);
+
+
