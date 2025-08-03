bb20cafefac3a1402f5a82e6d6ed6feba52d9feb
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-08-03 21:52:07 +0600

    added all file update

---
+++ scripts/eventLoop.js
@@ -0,0 +1,85 @@
+// // function a() {
+// //     b()
+// //     console.log("a")
+// // }
+// // function b() {
+// //     c()
+// //     console.log("b")
+// // }
+// // console.log("Hello");
+// // function c() {
+// //     d()
+// //     console.log("c")
+// // }
+
+// // setTimeout(() => {
+// //      console.log("ami setTimeout")
+// // },3000)
+
+// // setTimeout(() => {
+// //     console.log("ami setTimeout 2")
+// // },4000)
+
+// // function d() {
+// //     e()
+// //     console.log("d")
+// // }
+// // function e() {
+// //     f()
+// //     console.log("e")
+// // }
+// // function f() {
+  
+// //     console.log("f")
+// // }
+
+// // for (let i = 0; i < 5; i++) {
+// //     a();
+// // }
+
+
+
+
+// function a() {
+//     b();
+//     console.log("a");
+// }
+
+// function b() {
+//     c();
+//     console.log("b");
+// }
+
+// console.log("Hello");
+
+// function c() {
+//     d();
+//     console.log("c");
+// }
+
+// setTimeout(() => {
+//     console.log("ami setTimeout");
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log("ami setTimeout 2");
+// },4000)
+
+// function d() {
+//     e();
+//     console.log("d");
+// }
+
+// function e() {
+//     f();
+//     console.log("e")
+// }
+
+// function f() {
+//     console.log("f")
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     a();
+// }
+
