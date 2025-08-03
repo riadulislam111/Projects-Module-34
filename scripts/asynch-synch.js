8632a5969d0777c76d621f6c4a34c0f85050b5b9
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-08-03 12:21:37 +0600

    added all file update

---
+++ scripts/asynch-synch.js
@@ -0,0 +1,23 @@
+// const loadData = async () => {
+//     try {
+//         console.log("ami prothom")
+//         console.log("ami second")
+//         const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//         const data = await response.json();
+//         console.log(data)
+//         const result = 10 + 10;
+//         console.log(result)
+//         console.log("ami third")
+//     }
+//     catch {
+//         console.log("error")
+//     }
+// }
+// loadData();
+
+
+// const loadData = async () => {
+//     try
+// } 
+
+
