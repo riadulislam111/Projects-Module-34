b715d1ec9d9d2bed0ab6424fbacf506992fc0dfa
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-08-04 16:56:49 +0600

    added all fileupdate

---
+++ scripts/practic-task.js
@@ -0,0 +1,48 @@
+// function displayMessage() {
+//     setTimeout(() => {
+//         console.log("This message is displayed after 5 seconds");
+//     }, 5000);
+// }
+
+// displayMessage();
+
+// function displayMessage2() {
+//     setTimeout(() => {
+//         console.log("This message is displayed after 5 seconds")
+//     }, 5000);
+// }
+
+// displayMessage2();
+
+// function displayMessage3() {
+//     setTimeout(() => {
+//         console.log("This message is displayed after 5 seconds");
+//     }, 5000);
+// }
+
+// displayMessage3();
+
+
+// function displayMessage4() {
+//     setTimeout(() => {
+//         console.log("this is a 4numbar message");
+//     }, 5000);
+// }
+
+// displayMessage4();
+
+
+// function displayMessage5() {
+//     setTimeout(() => {
+//         console.log("This message is a five number message");
+//     }, 5000);
+// }
+
+// displayMessage5();
+
+
+
+//////
+
+
+
